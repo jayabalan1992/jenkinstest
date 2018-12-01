13fba112240cf261a9528649764793f94bfc2850
jayabalan <jayabalanbala@gmail.com>
2018-11-30 23:04:34 -0600

    Added terraform file to auto build ami using jenkins

---
+++ jenkinsami.tf
@@ -0,0 +1,54 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "key_name" {}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+ region     = "us-east-1"
+ key_name   = "new2018"
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+resource "aws_security_group" "jenkinssg" {
+  name = "security_group_for_node_app_websg"
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_ami" "jenkins_ami_number" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["jenkins-master*"]
+  }
+}
+
+resource "aws_instance" "jenkinsMaster" {
+  ami           = "${data.aws_ami.jenkins_ami_number.id}"
+  instance_type = "t2.micro"
+  count = 1
+  security_groups = ["${aws_security_group.jenkinssg.id}"]
+  associate_public_ip_address = true
+  key_name        = "${var.key_name}"
+}
+###########################################################################
+#                                                                         #
+#                                    Output                               #
+###########################################################################
